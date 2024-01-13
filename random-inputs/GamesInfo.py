fa401f901da814c70c07c6584400731b1bee5feb
Marexillius <112189645+Marexillius@users.noreply.github.com>
2024-01-13 16:23:31 -0500

    init

---
+++ random-inputs/GamesInfo.py
@@ -0,0 +1,70 @@
+from ButtonVariables import *
+
+##################### GAMECUBE #####################
+# Additional inputs to do more actions
+HOLD_A = C
+GROUNDPOUND = D
+CIRCLE = E
+gcnInputs = [A, HOLD_A, GROUNDPOUND, B, X, Y, L, R, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, CIRCLE]
+
+# GCN Function
+def gcnOutput(outButton):
+    # If the input is left, right, up or down
+    if outButton == LEFT_ARROW or outButton == UP_ARROW or outButton == RIGHT_ARROW or outButton == DOWN_ARROW:
+        HoldAndReleaseKey(outButton, 0.7)
+
+    # Button A Held
+    elif outButton == HOLD_A:
+        HoldAndReleaseKey(A, 1)
+
+    # Makes a Groudpound for Mario Games
+    elif outButton == GROUNDPOUND:
+        HoldAndReleaseKey(A,0.2)
+        time.sleep(0.3)
+        HoldAndReleaseKey(A,0.2)
+
+    # Turns in a circle
+    elif outButton == CIRCLE:
+        MultiKeys(DOWN_ARROW, RIGHT_ARROW, 0.2)
+        MultiKeys(DOWN_ARROW, LEFT_ARROW, 0.2)
+        MultiKeys(UP_ARROW, LEFT_ARROW, 0.2)
+        MultiKeys(UP_ARROW, RIGHT_ARROW, 0.2)
+
+    # If the input is a button (A, B, X, Y, R or L)
+    else:
+        HoldAndReleaseKey(outButton, 0.2)
+
+##################### N64 #####################
+n64Inputs = [A, GROUNDPOUND, HOLD_A, B, L, R, LEFT_ARROW, UP_ARROW, DOWN_ARROW, RIGHT_ARROW, CIRCLE]
+
+# N64 function
+def n64Output(outButton):
+    # If the input is left, right, up or down
+    if outButton == LEFT_ARROW or outButton == UP_ARROW or outButton == RIGHT_ARROW or outButton == DOWN_ARROW:
+        HoldAndReleaseKey(outButton, 0.7)
+
+    # Button A Held
+    elif outButton == HOLD_A:
+        HoldAndReleaseKey(A, 1)
+
+    # Makes a Groudpound for Mario Games
+    elif outButton == GROUNDPOUND:
+        HoldAndReleaseKey(A,0.2)
+        time.sleep(0.3)
+        HoldAndReleaseKey(A,0.2)
+
+    # Turns in a circle
+    elif outButton == CIRCLE:
+        MultiKeys(DOWN_ARROW, RIGHT_ARROW, 0.2)
+        MultiKeys(DOWN_ARROW, LEFT_ARROW, 0.2)
+        MultiKeys(UP_ARROW, LEFT_ARROW, 0.2)
+        MultiKeys(UP_ARROW, RIGHT_ARROW, 0.2)
+
+    # If the input is a button (A, B, R or L)
+    else:
+        HoldAndReleaseKey(outButton, 0.2)
+####################################################
+
+# Used list of games
+gameList = {"GCN": gcnInputs,
+            "N64": n64Inputs}
