c410114c99c625cf192a68d03d119242d5d57a77
Marexillius <alexandregervaisme@gmail.com>
2024-01-22 15:42:05 -0500

    added an untitled section for easy add

--- random-inputs/GamesInfo.py
+++ random-inputs/GamesInfo.py
@@ -63,7 +63,37 @@ def n64Output(outButton):
     # If the input is a button (A, B, R or L)
     else:
         HoldAndReleaseKey(outButton, 0.2)
-####################################################
+
+##################### IDK #####################
+untitledInputs = []
+
+# untitled function
+def untitledOutput(outButton):
+    # If the input is left, right, up or down
+    if outButton == LEFT_ARROW or outButton == UP_ARROW or outButton == RIGHT_ARROW or outButton == DOWN_ARROW:
+        HoldAndReleaseKey(outButton, 0.7)
+
+    # Button A Held
+    elif outButton == HOLD_A:
+        HoldAndReleaseKey(A, 1)
+
+    # Makes a Groudpound for Mario Games
+    elif outButton == GROUNDPOUND:
+        HoldAndReleaseKey(A,0.2)
+        time.sleep(0.3)
+        HoldAndReleaseKey(A,0.2)
+
+    # Turns in a circle
+    elif outButton == CIRCLE:
+        MultiKeys(DOWN_ARROW, RIGHT_ARROW, 0.2)
+        MultiKeys(DOWN_ARROW, LEFT_ARROW, 0.2)
+        MultiKeys(UP_ARROW, LEFT_ARROW, 0.2)
+        MultiKeys(UP_ARROW, RIGHT_ARROW, 0.2)
+
+    # If the input is a button (A, B, R or L)
+    else:
+        HoldAndReleaseKey(outButton, 0.2)
+##############################################
 
 # Used list of games
 gameList = {"GCN": gcnInputs,
